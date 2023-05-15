4d12a6693f2eec72f6e62747933717190759ebcf
Tim Knight <tim.knight1@engineering.digital.dwp.gov.uk>
2023-05-15 13:45:28 +0100

    chore: add tests for version 1 reports
    - https://github.com/IBM/openapi-validator version 1 produces different JSON to v0
    
    Signed-off-by: Tim Knight <tim.knight1@engineering.digital.dwp.gov.uk>

--- test/bin/index.test.js
+++ test/bin/index.test.js
@@ -13,15 +13,39 @@ describe("index.js tests", () => {
       execSync("npm uninstall --global");
     });
 
-    test("When passed a valid OpenAPI Validator report file then creates a valid GitLab Code Quality report file", () => {
+    test("When passed a valid OpenAPI Validator report file produced by Version 0, then creates a valid GitLab Code Quality report file", () => {
       const validatorReportPath = join(
         __dirname,
-        "/data/openapi-validator-report.json"
+        "/data/openapi-validator-report-v0.json"
       );
       const glReportPath = join(__dirname, "/data/gl-code-quality-report.json");
       const expectGLReportPath = join(
         __dirname,
-        "/data/expected-gl-code-quality-report.json"
+        "/data/expected-gl-code-quality-report-v0.json"
+      );
+
+      execSync(
+        `gl-code-quality-openapi-validator -s ./openapi-specification.yml -i ${validatorReportPath} -o ${glReportPath}`
+      );
+
+      const bufferActual = readFileSync(glReportPath);
+      const gitLabReportActual = JSON.parse(bufferActual);
+
+      const bufferExpected = readFileSync(expectGLReportPath);
+      const gitLabReportExpected = JSON.parse(bufferExpected);
+
+      expect(gitLabReportActual).toStrictEqual(gitLabReportExpected);
+    });
+
+    test("When passed a valid OpenAPI Validator report file produced by Version 1, then creates a valid GitLab Code Quality report file", () => {
+      const validatorReportPath = join(
+        __dirname,
+        "/data/openapi-validator-report-v1.json"
+      );
+      const glReportPath = join(__dirname, "/data/gl-code-quality-report.json");
+      const expectGLReportPath = join(
+        __dirname,
+        "/data/expected-gl-code-quality-report-v1.json"
       );
 
       execSync(
